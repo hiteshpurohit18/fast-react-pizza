5cf3f9a27f0ea90c9f49dd569bbcb3d24c01c937
Hitesh Purohit <purohithitesh579@gmail.com>
2025-12-31 11:24:28 +0530

    Initial Fast React Pizza project structure

---
+++ src/features/user/Username.jsx
@@ -0,0 +1,16 @@
+import { useSelector } from "react-redux";
+import { getUser } from "./userSlice";
+
+function Username() {
+  const username = useSelector(getUser);
+
+  if (!username) return null;
+
+  return (
+    <div className="hidden text-sm font-semibold text-red-50 md:block">
+      {username}
+    </div>
+  );
+}
+
+export default Username;
